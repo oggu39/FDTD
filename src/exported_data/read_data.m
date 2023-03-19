e02925087bab4e1285db384e8f51e280735c7380
oggu39 <128307503+oggu39@users.noreply.github.com>
2023-03-19 16:27:39 +0100

    Delete read_data.m

--- src/exported_data/read_data.m
+++
@@ -1,9 +0,0 @@
-function [reflection_data, transmission_data,source_data, t_data] = read_data(reflection_file, transmission_file, source_file, t_file)
-
-    reflection_data = readmatrix(reflection_file);
-    transmission_data = readmatrix(transmission_file);
-    source_data = readmatrix(source_file);
-    t_data = readmatrix(t_file);
-    
-
-end
